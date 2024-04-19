a183697f3524884328c8590b9e16b0db51be9758
Paroooo <jaffarbasmala04@gmail.com>
2024-04-19 09:05:48 +0800

    Create automated test for formatMoney

--- script/utils/money-format.js
+++ script/utils/money-format.js
@@ -1,3 +1,6 @@
-export default function moneyFormat(value) {
-   return (Math.round(value) / 100).toFixed(2)
-} 
+function moneyFormat(value = '') {
+   value = Math.round(value)
+   return (value * 0.01).toFixed(2)
+}
+
+export default moneyFormat
---
+++ script/utils/tests/moneyTest.js
@@ -0,0 +1,20 @@
+import moneyFormat from '../money-format.js';
+
+
+console.group('Money format test');
+testFunction(moneyFormat(2000.5), '20.01', 'Rounds up to the nearest cents?')
+testFunction(moneyFormat(2000.4), '20.00', 'Rounds down to the nearest cents?')
+testFunction(moneyFormat(2095), '20.95', 'It converts cents to dollars?')
+testFunction(moneyFormat(0), '0.00', 'It works with 0?')
+console.groupEnd('Money format test');
+
+
+
+function testFunction(funcToTest = new Function(), expectedValue, message) {
+   if (funcToTest === expectedValue) {
+      console.log('PASSED');
+   }
+   else {
+      console.error('FAILED');
+   }
+}
