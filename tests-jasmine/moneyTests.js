9ac5a7c44f1f759e2b83e6b9beae05fc47c60fe4
Paroooo <jaffarbasmala04@gmail.com>
2024-04-20 08:21:47 +0800

    Create test for money currency format

---
+++ tests-jasmine/moneyTests.js
@@ -0,0 +1,21 @@
+import moneyFormat from '../script/utils/money-format.js';
+
+
+describe('Test Suite: Money format', () => {
+
+   testThis('converts cent into dollar', moneyFormat(2095), '20.95')
+
+   testThis('rounds down to the nearest cent', moneyFormat(2000.4), '20.00')
+
+   testThis('rounds up to the nearest cent', moneyFormat(2000.5), '20.01')
+
+   testThis('works with 0', moneyFormat(0), '0.00')
+
+})
+
+
+function testThis(testName, callback, expectValue) {
+   it(testName, () => {
+      expect(callback).toEqual(expectValue)
+   })
+}
