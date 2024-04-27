bdf699a3964358f41ae14a60518b6d33caee3305
Paroooo <jaffarbasmala04@gmail.com>
2024-04-27 08:01:28 +0800

    Make private prop and method in class

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,16 +1,16 @@
 class Cart {
 
    cartItems
-   localStorageKey
+   #localStorageKey
 
    constructor(localStorageKey) {
-      this.localStorageKey = localStorageKey
+      this.#localStorageKey = localStorageKey
       this.loadFromStorage()
    }
 
 
    loadFromStorage() {
-      this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey))
+      this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey))
 
       if (!this.cartItems) {
          this.cartItems = [{
@@ -23,7 +23,7 @@ class Cart {
    }
 
    saveToStorage() {
-      localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems))
+      localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems))
    }
 
    addToCart(itemQuantity = 1, productId) {
